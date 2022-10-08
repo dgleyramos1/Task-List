04338745639ccdbebc42e7e6101f7f3fdbd731a6
Dgley Ramos <dgleysilva@gmail.com>
2022-10-08 15:05:36 -0300

    Criando a class Task com modelo

---
+++ src/main/java/com/taks/entity/Task.java
@@ -0,0 +1,64 @@
+package com.taks.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String task;
+
+    @Column(nullable = false)
+    private String description;
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the task
+     */
+    public String getTask() {
+        return task;
+    }
+
+    /**
+     * @param task the task to set
+     */
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
