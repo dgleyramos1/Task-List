c36a15a936747971fefe7deb11b5013868d27888
Dgley Ramos <dgleysilva@gmail.com>
2022-10-08 16:13:21 -0300

    Criando o metodo de delete

--- src/main/java/com/taks/controller/TaskController.java
+++ src/main/java/com/taks/controller/TaskController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +50,12 @@ public class TaskController {
         Task task = taskService.create(taskCreate);
         TaskDTO result = taskMapper.toTaskDTO(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation("Delete task")
+    public ResponseEntity<TaskDTO> delete(@PathVariable Long id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/taks/service/TaskService.java
+++ src/main/java/com/taks/service/TaskService.java
@@ -24,4 +24,8 @@ public class TaskService {
         return taskRepository.save(taskCreate);
     }
 
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
