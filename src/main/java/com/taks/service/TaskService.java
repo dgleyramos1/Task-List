cf2a66ff9bf9b0fd4848c229bd70558266582dd7
Dgley Ramos <dgleysilva@gmail.com>
2022-10-08 15:15:35 -0300

    Criando class de serviços

---
+++ src/main/java/com/taks/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.taks.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.taks.entity.Task;
+import com.taks.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+}
